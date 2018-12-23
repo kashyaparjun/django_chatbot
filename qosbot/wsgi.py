17811b961db1a1be4d846e8310505eb1148718c7
Arjun Kashyap <arjuns062009@gmail.com>
2018-12-23 15:49:15 +0530

    ok

--- qosbot/wsgi.py
+++ qosbot/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for qosbot project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/johhnybravo/django_chatbot'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qosbot.settings")
-
 application = get_wsgi_application()
+
+##ok
